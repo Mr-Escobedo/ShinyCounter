7067d9392f4378f429a1385469c00187b76d8c7f
Kevin Escobedo <escobedo001@gmail.com>
2020-03-25 13:12:54 -0700

    Added basic tests for Counter class

---
+++ C++/tests/counter_tests.cpp
@@ -0,0 +1,64 @@
+#include "counter_class.cpp"
+#include <gtest/gtest.h>
+
+TEST(CounterTest, goesUp)
+{
+	Counter c;
+	ASSERT_EQ(0, c.get_count());
+	c.up();
+	ASSERT_EQ(1, c.get_count());
+}
+
+TEST(CounterTest, goesDown)
+{
+	int origin_count = 10;
+	Counter c = Counter(origin_count, 1);
+	ASSERT_EQ(origin_count, c.get_count());
+	for(int i = 1; i < origin_count; i++)
+	{
+		c.down();
+		ASSERT_EQ(origin_count-i, c.get_count());
+	}
+}
+
+TEST(CounterTest, differentSteps)
+{
+	Counter c = Counter(0, 5);
+	ASSERT_EQ(0, c.get_count());
+	c.up();
+	ASSERT_EQ(5, c.get_count());
+	c.down();
+	ASSERT_EQ(0, c.get_count());
+}
+
+TEST(CounterTest, newCount)
+{
+	Counter c;
+	ASSERT_EQ(0, c.get_count());
+	c.reset_count(100);
+	ASSERT_EQ(100, c.get_count());
+	c.up();
+	c.reset_count(50);
+	ASSERT_EQ(50, c.get_count());
+}
+
+TEST(CounterTest, newStep)
+{
+	Counter c;
+	ASSERT_EQ(0, c.get_count());
+	for(int i = 0; i < 5; i++)
+	{
+		c.up();
+	}
+	ASSERT_EQ(5, c.get_count());
+
+	c.reset_step(5);
+	c.down();
+	ASSERT_EQ(0, c.get_count());
+}
+
+int main(int argc, char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
